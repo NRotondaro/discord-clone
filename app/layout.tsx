bdcc3907593a0fe7f3b972161e83444c55b01f97
NRotondaro <niksonrotondaro1@gmail.com>
2025-03-10 14:55:00 -0300

    feat: add real instrutions to run the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
         <body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
           <ThemeProvider
             attribute='class'
-            defaultTheme='dark'
-            enableSystem={false}
+            defaultTheme='system'
+            enableSystem
             storageKey='discord-theme'
           >
             {children}
