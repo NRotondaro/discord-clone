79b8cd0776d6c0b99b52f93bdaed9c867dbc700f
NRotondaro <niksonrotondaro1@gmail.com>
2025-03-18 15:40:58 -0300

    feat: add edit server functionality

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Server } from '@prisma/client';
 import { create } from 'zustand';
 
-export type ModalType = 'createServer' | 'invitePeople';
+export type ModalType = 'createServer' | 'invitePeople' | 'editServer';
 
 interface ModalData {
   server?: Server;
