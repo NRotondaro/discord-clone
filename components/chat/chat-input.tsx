381dbed2ac0bfcdec9879c0fa32f187477caf0a8
NRotondaro <niksonrotondaro1@gmail.com>
2025-03-28 09:24:41 -0300

    feat: implement message sending functionality

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,8 +1,11 @@
 'use client';
 
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import qs from 'query-string';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { Plus, Smile } from 'lucide-react';
 
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
@@ -26,8 +29,18 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
 
   const isLoading = form.formState.isSubmitting;
 
-  const onSubmit = async (value: z.infer<typeof formSchema>) => {
-    console.log(value);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const url = qs.stringifyUrl({
+        url: apiUrl,
+        query,
+      });
+
+      await axios.post(url, values);
+      form.reset();
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -36,14 +49,26 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
         <FormField
           control={form.control}
           name='content'
-          render={({}) => (
+          render={({ field }) => (
             <FormItem>
               <FormControl>
                 <div className='relative p-4 pb-6'>
                   <button
                     type='button'
                     onClick={() => {}}
-                    className='absolute left-8 top-7 flex h-6 w-6 items-center justify-center rounded-full bg-zinc-500 p-1 transition hover:bg-zinc-600 dark:bg-zinc-400 dark:hover:bg-zinc-300'></button>
+                    className='absolute left-8 top-7 flex h-6 w-6 items-center justify-center rounded-full bg-zinc-500 p-1 transition hover:bg-zinc-600 dark:bg-zinc-400 dark:hover:bg-zinc-300'>
+                    <Plus className='text-white dark:text-[#313338]' />
+                  </button>
+                  <Input
+                    autoComplete='off'
+                    disabled={isLoading}
+                    className='border-none bg-zinc-200/90 px-14 py-6 text-zinc-600 focus-visible:ring-0 focus-visible:ring-offset-0 dark:bg-zinc-700/75 dark:text-zinc-200'
+                    placeholder={`Message ${type === 'conversation' ? name : '#' + name}`}
+                    {...field}
+                  />
+                  <div className='absolute right-8 top-7'>
+                    <Smile />
+                  </div>
                 </div>
               </FormControl>
             </FormItem>
