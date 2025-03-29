21539cc33016d1fce47f92c0cc7df2b05ea60ef6
NRotondaro <niksonrotondaro1@gmail.com>
2025-03-29 09:43:05 -0300

    feat: implement API for fetching chat messages with pagination

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,7 +1,17 @@
 'use client';
 
-import { Member } from '@prisma/client';
+import { Member, Message, Profile } from '@prisma/client';
+import { Loader2, ServerCrash } from 'lucide-react';
+
 import { ChatWelcome } from './chat-welcome';
+import { useChatQuery } from '@/hooks/use-chat-query';
+import { Fragment } from 'react';
+
+type MessageWithMemberWithProfile = Message & {
+  member: Member & {
+    profile: Profile;
+  };
+};
 
 interface ChatMessagesProps {
   name: string;
@@ -26,10 +36,46 @@ export const ChatMessages = ({
   paramValue,
   type,
 }: ChatMessagesProps) => {
+  const queryKey = `chat:${chatId}`;
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } = useChatQuery({
+    queryKey,
+    apiUrl,
+    paramKey,
+    paramValue,
+  });
+
+  if (status === 'pending') {
+    return (
+      <div className='flex flex-1 flex-col items-center justify-center'>
+        <Loader2 className='my-4 h-7 w-7 animate-spin to-zinc-500' />
+        <p className='text-xs text-zinc-500 dark:text-zinc-400'>Loading messages...</p>
+      </div>
+    );
+  }
+
+  if (status === 'error') {
+    return (
+      <div className='flex flex-1 flex-col items-center justify-center'>
+        <ServerCrash className='my-4 h-7 w-7 to-zinc-500' />
+        <p className='text-xs text-zinc-500 dark:text-zinc-400'>Something went wrong!</p>
+      </div>
+    );
+  }
+
   return (
     <div className='flex flex-1 flex-col overflow-y-auto py-4'>
       <div className='flex-1' />
       <ChatWelcome type={type} name={name} />
+      <div className='mt-auto flex flex-col-reverse'>
+        {data?.pages?.map((group, i) => (
+          <Fragment key={i}>
+            {group.items.map((message: MessageWithMemberWithProfile) => (
+              <div key={message.id}>{message.content}</div>
+            ))}
+          </Fragment>
+        ))}
+      </div>
     </div>
   );
 };
